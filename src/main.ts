ff90713fe68265fefa44e0336ded9a99ecd21c50
Paul O’Shannessy <paul@oshannessy.com>
2019-09-14 15:50:54 -0700

    First pass
    Using `ncc` instead of checking in node modules. If it's good enough for
    zeit now, it's good enough for me.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,42 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-async function run() {
+const LABEL_TO_USERS_MAP: { [key: string]: string[] } = {
+  bug: ['@zpao-test'],
+  duplicate: ['@zpao']
+};
+
+async function main() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const repoToken: string = core.getInput('repo-token', { required: true });
+    const issue: { owner: string; repo: string; number: number } =
+      github.context.issue;
+
+    const client = new github.GitHub(repoToken);
+    // TODO: can we just get the labels from the context?
+    const { data: labels } = await client.issues.listLabelsOnIssue({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number
+    });
+
+    let mentionees: string[] = [];
+    labels.forEach(label => {
+      const users = LABEL_TO_USERS_MAP[label.name];
+      if (users != null) {
+        mentionees = mentionees.concat(users);
+      }
+    });
+
+    await client.issues.createComment({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      body: `cc ${mentionees.join(', ')}`
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run();
+main();
